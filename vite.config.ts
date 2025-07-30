d46235fa7e6ed1c89b9053b372a7dc2e05f770e8
Moussa Saidi <moussa.saidi.01@gmail.com>
2025-07-30 15:11:23 +0100

    feat: base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/sotherma-contracts/" : "/",
+  base: "/sotherma-contracts/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {

b15095acf4d3c0bbec42af88ed9c9c176d53a418
Moussa Saidi <moussa.saidi.01@gmail.com>
2025-08-02 13:10:11 +0100

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,9 @@ docker rm -f react-ui
 # Rebuild and run
 docker build -t sotherma-contracts .
 docker run -d -p 5173:3000 --restart always --name react-ui sotherma-contracts
+
+# Remove old images
+docker system prune -af
+
+# Reload Nginx to apply changes
+sudo systemctl reload nginx
